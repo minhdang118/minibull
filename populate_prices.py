d30ad2c70a4ebe5495c0163b2edda2fa22cbba76
minhdang118 <mdang2@u.rochester.edu>
2024-09-27 12:58:46 +0700

    some changes

--- populate_prices.py
+++
@@ -1,8 +0,0 @@
-import config
-import alpaca_trade_api as tradeapi
-from alpaca_trade_api.rest import TimeFrame
-
-api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, config.API_URL)
-
-barsets = api.get_bars(['AAPL', 'MSFT'], TimeFrame.Hour)
-print(barsets)
