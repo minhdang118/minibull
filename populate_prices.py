d075dd36cf75a0329fe69b6bc392ceced49087c2
minhdang118 <mdang2@u.rochester.edu>
2024-10-02 10:35:07 +0700

    populated stock prices for one year

--- populate_prices.py
+++ populate_prices.py
@@ -1,16 +1,44 @@
+from datetime import datetime, timedelta
 import sqlite3, config
+from zoneinfo import ZoneInfo
 
-from alpaca.data.historical import CryptoHistoricalDataClient
-from alpaca.data.requests import CryptoBarsRequest
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 
-client = CryptoHistoricalDataClient()
-request_params = CryptoBarsRequest(
-  symbol_or_symbols=["BTC/USD"],
-  timeframe=TimeFrame.Day,
-  start="2024-09-01",
-  end="2024-09-07"
-)
+# Connect to the database
+connection = sqlite3.connect(config.DB_FILE)
+connection.row_factory = sqlite3.Row
 
-btc_bars = client.get_crypto_bars(request_params)
-print(btc_bars.df)
+cursor = connection.cursor()
+
+cursor.execute("""
+    SELECT id, symbol, name FROM stock
+""")
+rows = cursor.fetchall()
+
+symbols = [row['symbol'] for row in rows]
+
+stock_historical_data_client = StockHistoricalDataClient(config.API_KEY, config.SECRET_KEY, url_override = None)
+now = datetime.now(ZoneInfo("America/New_York"))
+
+chunks = [symbols[i:i + 200] for i in range(0, len(symbols), 200)]
+
+for chunk in chunks:
+  request = StockBarsRequest(
+      symbol_or_symbols=chunk,
+      timeframe=TimeFrame(amount=1, unit=TimeFrameUnit.Day),
+      start=now - timedelta(days=365),
+      end=now - timedelta(days=1),
+  )
+  barsets = stock_historical_data_client.get_stock_bars(request)
+  for symbol in barsets.data:
+      print(f"Processing {symbol}")
+      for bar in barsets.data[symbol]:
+          cursor.execute("""
+              INSERT INTO stock_price (stock_id, date, open, high, low, close, volume)
+              VALUES (?, ?, ?, ?, ?, ?, ?)
+          """, (rows[symbols.index(symbol)]['id'], bar.timestamp.date(), bar.open, bar.high, bar.low, bar.close, bar.volume))
+
+connection.commit()
--- stock_data.py
+++ stock_data.py
@@ -7,17 +7,21 @@ from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 
-symbol = 'AAPL'
+symbols = ['AAPL']
 
 stock_historical_data_client = StockHistoricalDataClient(config.API_KEY, config.SECRET_KEY, url_override = None)
 
 now = datetime.now(ZoneInfo("America/New_York"))
 request = StockBarsRequest(
-    symbol_or_symbols=[symbol],
+    symbol_or_symbols=symbols,
     timeframe=TimeFrame(amount=1, unit=TimeFrameUnit.Hour),
     start=now - timedelta(days=5),
     limit=1000
 )
 
-bars = stock_historical_data_client.get_stock_bars(request)
-print(bars.df)
+barsets = stock_historical_data_client.get_stock_bars(request)
+
+for symbol in barsets.data:
+    print(symbol)
+    for bar in barsets.data[symbol]:
+        print(f"Timestamp: {bar.timestamp}, Open: {bar.open}, High: {bar.high}, Low: {bar.low}, Close: {bar.close}, Volume: {bar.volume}")
